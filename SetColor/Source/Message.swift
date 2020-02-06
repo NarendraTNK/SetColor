606fd3a71c6694a2d0720d43a7e562292ef667a2
Narendra <narendratnk11@gmail.com>
2020-02-06 11:48:56 +0800

    localization added

---
+++ SetColor/Source/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  SetColor
+//
+//  Created by hfcb on 2/6/20.
+//  Copyright © 2020 hfcb. All rights reserved.
+//
+
+import Foundation
+
+struct public Identifier {
+    static public let firstname = "First Name"
+    static public let lastname = "Last Name"
+    static public let address = "Address"
+    static public let go = "Go"
+}
--- SetColor/Source/ViewController/StartViewController.swift
+++ SetColor/Source/ViewController/StartViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 public class StartViewController: UIViewController {
 
+    //MARK:- IBOutlet
+    @IBOutlet weak public var languageSegmentControl: UISegmentedControl!
+    @IBOutlet weak public var firstNameLabel: UILabel!
+    @IBOutlet weak public var lastNameLabel: UILabel!
+    @IBOutlet weak public var addressLabel: UILabel!
+    @IBOutlet weak public var gobtnOutlet: UIButton!
+    
+    
+    //MARK:- View Cycle
     override public func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        firstNameLabel.text = Identifier.firstname
+        lastNameLabel.text = Identifier.lastname
+        addressLabel.text = Identifier.address
+        gobtnOutlet.setTitle(Identifier.go, for: .normal)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK:- IBAction
+    @IBAction public func OnClickSegmentControl(_ sender: UISegmentedControl) {
+        
+        if (sender.selectedSegmentIndex == 0){
+            
+        }else if (sender.selectedSegmentIndex == 1){
+            
+        }else if (sender.selectedSegmentIndex == 2){
+            
+        }else if (sender.selectedSegmentIndex == 3){
+            
+        }
+   
     }
-    */
+    @IBAction public func OnClickGoButton(_ sender: UIButton) {
+        
+        
+    }
 
 }
